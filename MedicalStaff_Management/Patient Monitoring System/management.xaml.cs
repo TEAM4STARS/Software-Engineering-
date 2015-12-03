fd72f49c56e79cf86b37f81a79ba6a40fbfbad38
waltermaks <waltermakoni@live.co.uk>
2015-12-03 11:46:11 +0000

    Draft medicalStaff_Management

---
+++ MedicalStaff_Management/Patient Monitoring System/management.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.OleDb;
+using System.Configuration;
+
+namespace Patient_Monitoring_System
+{
+    /// <summary>
+    /// Interaction logic for management.xaml
+    /// </summary>
+    public partial class management : Window
+    {
+        public management()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection dataConnection = new OleDbConnection(System.Configuration.ConfigurationManager.ConnectionStrings["db"].ConnectionString);
+        MsgBox ErrorMsg = new MsgBox();
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            Patient_Monitoring_System.DatabaseDataSet databaseDataSet = ((Patient_Monitoring_System.DatabaseDataSet)(this.FindResource("databaseDataSet")));
+            // Load data into the table Medical_Staff. You can modify this code as needed.
+            Patient_Monitoring_System.DatabaseDataSetTableAdapters.Medical_StaffTableAdapter databaseDataSetMedical_StaffTableAdapter = new Patient_Monitoring_System.DatabaseDataSetTableAdapters.Medical_StaffTableAdapter();
+            databaseDataSetMedical_StaffTableAdapter.Fill(databaseDataSet.Medical_Staff);
+            System.Windows.Data.CollectionViewSource medical_StaffViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("medical_StaffViewSource")));
+            medical_StaffViewSource.View.MoveCurrentToFirst();
+            // Load data into the table Alarm. You can modify this code as needed.
+            Patient_Monitoring_System.DatabaseDataSetTableAdapters.AlarmTableAdapter databaseDataSetAlarmTableAdapter = new Patient_Monitoring_System.DatabaseDataSetTableAdapters.AlarmTableAdapter();
+            databaseDataSetAlarmTableAdapter.Fill(databaseDataSet.Alarm);
+            System.Windows.Data.CollectionViewSource alarmViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("alarmViewSource")));
+            alarmViewSource.View.MoveCurrentToFirst();       
+        }
+
+        private void menu_Click(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+            this.Hide();
+        }
+
+        private void btn_staff_search_Click(object sender, RoutedEventArgs e)
+        {
+              try {
+                OleDbCommand Query = new OleDbCommand();
+                Query.CommandText = "select * from Medical_Staff where Staff_ID = "+ int.Parse(txtStaffSearch.Text);
+                Query.Connection = dataConnection;  
+                Query.Connection.Open();
+                Query.ExecuteNonQuery();
+                DataTable dt = new DataTable();               
+                OleDbDataAdapter adapt = new OleDbDataAdapter(Query);
+                adapt.Fill(dt);
+                medical_StaffDataGrid.DataContext = dt;
+                Query.Connection.Close();
+            }
+
+            catch (Exception ex)
+            {
+                ErrorMsg.ErrorMsgBox(ex);
+            }
+ 
+            
+        }
+    }
+}
---
+++ MedicalStaff_Management/Patient Monitoring System/medicalStaff.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Shapes;
+using System.Configuration;
+
+namespace Patient_Monitoring_System
+{
+    /// <summary>
+    /// Interaction logic for medicalStaff.xaml
+    /// </summary>
+    public partial class medicalStaff : Window
+    {
+        public medicalStaff()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection dataConnection = new OleDbConnection(System.Configuration.ConfigurationManager.ConnectionStrings["db"].ConnectionString); // this specifies the connection string (where the connection is stored)
+
+        MsgBox ErrorMsg = new MsgBox();
+        MsgBox ValidationMsg = new MsgBox();
+
+        public object Connection { get; private set; }
+
+        private void start_shift_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime myTime = DateTime.Now;
+            string StaffID;
+            string EmailNumber;
+            string Fullname;
+            StaffID = txtStaffID.Text;
+            EmailNumber = txtNumberEmail.Text;
+            Fullname = txtFullname.Text;
+
+            if (StaffID == "" || EmailNumber == "" || Fullname == "") //if the button is pressed and the fields are empty show message box......
+            {
+                ValidationMsg.ValidationMsgBox(); //gets the field vaidation message from the MsgBox class
+            }
+
+            else
+            {
+                try  //The code inside this try block may cause the exception and it is executed until an exception is thrown or it has completed successfully.
+                {
+                    
+
+                    OleDbCommand Query = new OleDbCommand();
+                    
+                    Query.CommandText = "INSERT INTO Medical_Staff (Staff_ID, Full_Name, Email_or_Number, Time_In) VALUES ('" + txtStaffID.Text + "', '" + txtFullname.Text + "', '" + txtNumberEmail.Text + "', '" + myTime.ToString() + "')";
+                    Query.Connection = dataConnection;
+                    Query.Connection.Open();
+                    Query.ExecuteNonQuery();
+                    Query.Connection.Close();
+                    MessageBox.Show("You are starting your shift at " + myTime.ToShortTimeString(), "STARTING SHIFT", MessageBoxButton.OK, MessageBoxImage.Information);
+                    txtStaffID.Text     ="";
+                    txtNumberEmail.Text ="";
+                    txtFullname.Text    ="";
+                }
+
+                catch (Exception ex)
+                {
+                    ErrorMsg.ErrorMsgBox(ex); //gets the exeption error message from the MsgBox class when the catch throws an error
+                }
+            }
+        }
+
+        private void end_shift_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime myTime = DateTime.Now;  //gets the local time
+            string StaffID;
+            string EmailNumber;
+            string Fullname;
+            StaffID = txtStaffID.Text;
+            EmailNumber = txtNumberEmail.Text;
+            Fullname = txtFullname.Text;
+
+            if (StaffID == "" )
+            {
+                ValidationMsg.ValidationMsgBox(); //gets this from the MsgBox class
+            }
+            
+            else
+            {
+                try //The code inside this try block may cause the exception and it is executed until an exception is thrown or it has completed successfully.
+                {
+                    OleDbCommand Query = new OleDbCommand();
+                    Query.CommandText = "UPDATE Medical_Staff SET Time_Out = '" + myTime.ToString() + "' WHERE Staff_ID =" + txtStaffID.Text + ""; //sql command to be executed
+                    Query.Connection = dataConnection; //gets connection of data source
+                    Query.Connection.Open(); // opens the connection to the database
+                    Query.ExecuteNonQuery(); // executes the query
+                    Query.Connection.Close(); // closes the connection to the database
+                    MessageBox.Show("You are ending your shift at " + myTime.ToShortTimeString(), "ENDING SHIFT", MessageBoxButton.OK, MessageBoxImage.Information);
+                    txtStaffID.Text = "";
+                }
+
+                catch (Exception ex) //catches an exception
+                {
+                    ErrorMsg.ErrorMsgBox(ex);  //gets the exeption error message from the MsgBox class
+                }
+
+                finally
+                {
+
+                }
+          
+            }
+          }
+
+        private void menu_Click(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show(); //shows the main window which runs first when the program is run 
+            this.Hide(); //then hides this window
+        }
+    }
+}
