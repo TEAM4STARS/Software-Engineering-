7bdfed1f28c7a0e6016f77f7ccb2e9bbb37c69a4
waltermaks <waltermakoni@live.co.uk>
2015-12-13 09:24:40 +0000

    Patient monitor system
    alarmer not fully functional

---
+++ PatientMonitorSystem/PatientMonitor/PatientDataReader.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+
+namespace PatientMonitor
+{
+    public class PatientDataReader
+    {
+        StreamReader dataFile;
+        StreamReader dataFile1;
+        StreamReader dataFile2;
+        StreamReader dataFile3;
+        StreamReader dataFile4;
+        StreamReader dataFile5;
+        StreamReader dataFile6;
+        StreamReader dataFile7;
+        StreamReader dataFile8;
+
+        /// <summary>
+        /// Initializes a new unconnected instance.
+        /// </summary>
+        public PatientDataReader()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PMS.PatientDataReader"/> class.
+        /// </summary>
+        /// <param name="fileName">File name.</param>
+        public PatientDataReader(string fileName)
+        {
+            // Open the file
+            dataFile = new StreamReader(fileName);
+            // Discard the headings
+            dataFile.ReadLine();
+        }
+
+        public void Connect( string fileName1, string fileName2, string fileName3, string fileName4, string fileName5, string fileName6, string fileName7, string fileName8)
+        {
+            // Open the file
+            dataFile1 = new StreamReader(fileName1);
+            dataFile2 = new StreamReader(fileName2);
+            dataFile3 = new StreamReader(fileName3);
+            dataFile4 = new StreamReader(fileName4);
+            dataFile5 = new StreamReader(fileName5);
+            dataFile6 = new StreamReader(fileName6);
+            dataFile7 = new StreamReader(fileName7);
+            dataFile8 = new StreamReader(fileName8);
+            
+           
+            dataFile1.ReadLine();
+            dataFile2.ReadLine();
+            dataFile3.ReadLine();
+            dataFile4.ReadLine();
+            dataFile5.ReadLine();
+            dataFile6.ReadLine();
+            dataFile7.ReadLine();
+            dataFile8.ReadLine();
+           
+        }
+
+        public string getData()
+        {
+            return dataFile.ReadLine();
+        }
+        public string getData1()
+        {
+            return dataFile1.ReadLine();
+        }
+        public string getData2()
+        {
+            return dataFile2.ReadLine();
+        }
+        public string getData3()
+        {
+            return dataFile3.ReadLine();
+        }
+        public string getData4()
+        {
+            return dataFile4.ReadLine();
+        }
+        public string getData5()
+        {
+            return dataFile5.ReadLine();
+        }
+        public string getData6()
+        {
+            return dataFile6.ReadLine();
+        }
+        public string getData7()
+        {
+            return dataFile7.ReadLine();
+        }
+        public string getData8()
+        {
+            return dataFile8.ReadLine();
+
+            if (dataFile8.EndOfStream)
+            { 
+            return dataFile8.ReadLine();
+            }
+           
+        }
+        
+    }
+}
---
+++ PatientMonitorSystem/PatientMonitor/monitoringandalarmdetails.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Media;
+
+namespace PatientMonitor
+{
+    /// <summary>
+    /// Interaction logic for monitoringandalarmdetails.xaml
+    /// </summary>
+    public partial class monitoringandalarmdetails :Window
+    {
+        readonly MainWindow _mainWindow = null;
+        readonly IPatientFactory _patientFactory  = new PatientFactory();
+
+        PatientAlarmer _alarmer;
+        CheckBox AlarmMuter;
+
+        SoundPlayer mutable = new SoundPlayer(PatientMonitor.Properties.Resources.Mutable);
+        SoundPlayer nonMutable = new SoundPlayer(PatientMonitor.Properties.Resources.NonMutable);
+
+
+        public monitoringandalarmdetails()
+        {
+            InitializeComponent();
+            setupComponents();
+            heartRateLower.AlarmValue = (int)DefaultSettings.LOWER_PULSE_RATE;
+            breathingRateLower.AlarmValue = (int)DefaultSettings.LOWER_BREATHING_RATE;
+            temperatureLower.AlarmValue = (int)DefaultSettings.LOWER_TEMPERATURE;
+            systolicLower.AlarmValue = (int)DefaultSettings.LOWER_SYSTOLIC;
+            diastolicLower.AlarmValue = (int)DefaultSettings.LOWER_DIASTOLIC;
+
+            heartRateUpper.AlarmValue = (int)DefaultSettings.UPPER_PULSE_RATE;
+            breathingRateUpper.AlarmValue = (int)DefaultSettings.UPPER_BREATHING_RATE;
+            temperatureUpper.AlarmValue = (int)DefaultSettings.UPPER_TEMPERATURE;
+            systolicUpper.AlarmValue = (int)DefaultSettings.UPPER_SYSTOLIC;
+            diastolicUpper.AlarmValue = (int)DefaultSettings.UPPER_DIASTOLIC;
+
+            heartRateLower.ValueChanged += new EventHandler(limitsChanged);
+            breathingRateLower.ValueChanged += new EventHandler(limitsChanged);
+            temperatureLower.ValueChanged += new EventHandler(limitsChanged);
+            systolicLower.ValueChanged += new EventHandler(limitsChanged);
+            diastolicLower.ValueChanged += new EventHandler(limitsChanged);
+
+            heartRateUpper.ValueChanged += new EventHandler(limitsChanged);
+            breathingRateUpper.ValueChanged += new EventHandler(limitsChanged);
+            temperatureUpper.ValueChanged += new EventHandler(limitsChanged);
+            systolicUpper.ValueChanged += new EventHandler(limitsChanged);
+            diastolicUpper.ValueChanged += new EventHandler(limitsChanged);
+
+        }
+
+        void limitsChanged(object sender, EventArgs e)
+        {
+            monitoringandalarmdetails monitor = new monitoringandalarmdetails();
+            
+            if (_alarmer == null)
+            {
+                _alarmer = (PatientAlarmer)_patientFactory.CreateandReturnObj(PatientClassesEnumeration.PatientAlarmer);
+            }
+            _alarmer.PulseRateTester.LowerLimit = heartRateLower.AlarmValue;
+            _alarmer.BreathingRateTester.LowerLimit = breathingRateLower.AlarmValue;
+            _alarmer.TemperatureTester.LowerLimit = temperatureLower.AlarmValue;
+            _alarmer.SystolicBpTester.LowerLimit = systolicLower.AlarmValue;
+            _alarmer.DiastolicBpTester.LowerLimit = diastolicLower.AlarmValue;
+
+            _alarmer.PulseRateTester.UpperLimit = heartRateUpper.AlarmValue;
+            _alarmer.BreathingRateTester.UpperLimit = breathingRateUpper.AlarmValue;
+            _alarmer.TemperatureTester.UpperLimit = temperatureUpper.AlarmValue;
+            _alarmer.SystolicBpTester.UpperLimit = systolicUpper.AlarmValue;
+            _alarmer.DiastolicBpTester.UpperLimit = diastolicUpper.AlarmValue;
+            
+        }
+
+        void setupComponents()
+        {
+            if (_alarmer == null)
+            {
+                _alarmer = (PatientAlarmer)_patientFactory.CreateandReturnObj(PatientClassesEnumeration.PatientAlarmer);
+            }
+            _alarmer = (PatientAlarmer)_patientFactory.CreateandReturnObj(PatientClassesEnumeration.PatientAlarmer);
+            _alarmer.BreathingRateAlarm += new EventHandler(soundMutableAlarm);
+            _alarmer.DiastolicBloodPressureAlarm += new EventHandler(soundMutableAlarm);
+            _alarmer.PulseRateAlarm += new EventHandler(soundMutableAlarm);
+            _alarmer.SystolicBloodPressureAlarm += new EventHandler(soundMutableAlarm);
+            _alarmer.TemperatureAlarm += new EventHandler(soundMutableAlarm);
+        }
+
+
+        void soundMutableAlarm(object sender, EventArgs e)
+        {
+            monitoringandalarmdetails monitor = new monitoringandalarmdetails();
+            if (AlarmMuter.IsChecked == false)
+            {
+                //monitor.soundMutableAlarm();
+            }
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+             Application.Current.Shutdown();
+        }
+
+        private void btnSendEMAIL_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Email sent!");
+        }
+
+        private void btnSendSMS_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("SMS sent!");
+        }
+    }
+}
